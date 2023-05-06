4815d6c9d1413fa51eef210cb7da733eb4a3dbcf
Azbayr <azbayrpurevtseren813@gmail.com>
2023-05-06 12:34:19 +0800

    add a get route for getting all categories and products by a specific category

--- server/v1/routes/categories/categoriesRouter.js
+++ server/v1/routes/categories/categoriesRouter.js
@@ -1,9 +1,49 @@
 const express = require('express');
 const categoriesRouter = express.Router();
+const Product = require('../../config/productsSchema');
 
-const { getCategories, } = require('../categoriesController.js');
 
-categoriesRouter.get('/', getCategories);
+categoriesRouter.get('/', async (req, res) => {
+    const categories = [
+        'Electronics',
+        'Clothing & Accessories',
+        'Home & Garden',
+        'Health & Beauty',
+        'Sports & Outdoor',
+        'Toys & Games',
+        'Books & Media',
+        'Food & Grocery',
+    ];
+
+    if (req.query.category) {
+        const category = req.query.category.replace('&', '&');
+        const products = await Product.find({ 
+            category: {
+                $regex: new RegExp(category, 'i'),
+            } 
+        });
+        if (products.length === 0) {
+            res.status(404).json({
+                status: 'fail',
+                message: 'No products found',
+            });
+        } else {
+            res.status(200).json({
+                status: 'success',
+                data: {
+                    products,
+                }
+            });
+        }
+    } else {
+        res.status(200).json({
+            status: 'success',
+            data: {
+                categories,
+            }
+        });
+    }
+});
 
 
 module.exports = categoriesRouter;

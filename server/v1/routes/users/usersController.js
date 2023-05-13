9688a86af252af8e24db3a4fbaa58e9925ade019
Azbayr <azbayrpurevtseren813@gmail.com>
2023-05-13 11:19:53 +0800

    fix error hanlers in controller functions

--- server/v1/routes/users/usersController.js
+++ server/v1/routes/users/usersController.js
@@ -1,11 +1,11 @@
 const User = require('../../config/usersSchema');
 
 const showAllUsers = async (req, res, next) => {
-    const users = await User.find({}, {_id: 0, hash: 0, salt: 0, __v: 0});
     try {
+        const users = await User.find({}, {_id: 0, hash: 0, salt: 0, __v: 0});
         res.status(200).json(users);
     } catch (err) {
-        res.status(401).json({ message: err.message });
+        res.status(500).json({ message: err.message });
     }   
 }
 
@@ -13,12 +13,15 @@ const showUserById = async (req, res, next) => {
     const { id } = req.params;
     try {
         const user = await User.findById(id, {_id: 0, hash: 0, salt: 0, __v: 0});
-        res.status(200).json(user);
+        if (user) {
+            res.status(200).json(user);
+        } else {
+            res.status(404).json({ message: "User does not exist" });
+        }
     } catch (err) {
-        res.status(404).json({ message: 'User does not exist' });
+        res.status(500).json({ message: err.message });
     }
 }
-
 
 module.exports = {
     showAllUsers,

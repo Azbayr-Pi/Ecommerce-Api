927a6fab355554b389861b7ecff3fd89123f487d
Azbayr <azbayrpurevtseren813@gmail.com>
2023-05-21 22:28:41 +0800

    import database url

--- server/v1/src/app.js
+++ server/v1/src/app.js
@@ -8,6 +8,7 @@ const categoriesRouter = require('../routes/categories/categoriesRouter');
 const reviewsRouter = require('../routes/reviews/reviewsRoute');
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
+const { connectionString } = require('../config/database'); 
 const loginRouter = require('../routes/Auth/loginRoute');
 const registerRouter = require('../routes/Auth/registerRoute');
 
@@ -16,14 +17,14 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
+console.log(connectionString);
 require('../config/passport');
 
 app.use(session({
     secret: 'user\'s session',
     resave: false,
     saveUninitialized: true,
-    store: MongoStore.create({ mongoUrl: "mongodb://127.0.0.1:27017/Ecommerce-Api"}),
+    store: MongoStore.create({ mongoUrl: connectionString }),
     cookie: {
         maxAge: 1000 * 60 * 60 * 24
     }
